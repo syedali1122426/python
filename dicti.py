af63212f427402ead73d02c169fa0ff825f2ff42
syedali1122426 <syedalishamsi47@ghamil.com>
2025-08-10 21:11:32 -0700

    read: loops + dict object

---
+++ dicti.py
@@ -0,0 +1,21 @@
+
+
+
+dict1 = {'a' : 1, 'b' : 2, 'c' : 3}
+
+dict1 = {'name' : 'age' : 30, 'email' : 'ali@gamil'.com, 'study': }
+                                                                    
+# dict1['a'] = 'ali'
+
+# print(dict1.get('a') )
+# dict1.pop('age')
+# x = dict1.values()
+# print(list(x))
+
+
+# get the values and get second highest Value
+current_grade = {2022: '80%', 2023: '90%', 2024: '70%', 2025: '85%'}
+# current_grade = list(current_grade.values())
+# print(list(current_grade))
+print(current_grade)
+result = sorted(current_grade,)
---
+++ loops.py
@@ -0,0 +1,28 @@
+
+
+# fruits = ["apple", "bnana", "cherry"]
+# fruits[2]
+# print(fruits[2])
+
+# for abv in fruits:
+    # print(abv)
+
+
+for ali in range(10, 10):
+ print(ali)
+ if ali == 5:
+   print("number is 5")
+else:
+ print("ali is here")
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+#   if x == "banana":
+#     break
+#   print(x)
+# adj = ["red", "big" "tasty"]
+# fruits = ["apple", "bnana", "cherry"]
+# for x in adj:
+#    for y in fruits:
+#      print(x,y)
