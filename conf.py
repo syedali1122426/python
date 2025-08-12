0453e028cc5da5bf2232c92a98541bd786dbfd3e
syedali1122426 <syedalishamsi47@ghamil.com>
2025-08-11 20:53:21 -0700

    read: if conditions

---
+++ conf.py
@@ -0,0 +1,51 @@
+
+
+
+a = 5
+b = 10
+
+# if a == b:
+#     print("a is equal to b")
+#     print(a == b)
+# elif a > b:
+#     print("a is not equal to b")
+#     print(a < b)
+#     print(a == b)
+# else:
+#     print("a is not equal to b")
+#     print( a > b)
+# print(a == b)
+
+
+# a = 335
+# b = 330
+# print("a") if a  < b else print("B")
+# print("A") if a > b else print("=") if a == b else print("8")
+
+
+# Arthamatic operator
+
+a = 5
+b = 10
+c = None
+
+# if a or b or c:
+#     print("Both a and b are non-zero")
+# else:
+#     print("At least one of a or b is zero")
+
+list1 = [5 , 10, 15, 20, 25]
+
+if 5 in list1:
+    print("5 is in the list")
+else:
+    print("5 is not in the list")
+
+
+x = 41
+
+# if x < 10:
+#     print("Above ten,")
+string_1 = "I am very  "
+if x > 20:
+        print("and also above 20!")
